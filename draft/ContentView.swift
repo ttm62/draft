20abc75764d9c2187eccd942b262dc48a1b35801
davidtam <ttm.unary@gmail.com>
2024-05-20 13:14:29 +0700

    Initial Commit

---
+++ draft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  draft
+//
+//  Created by davidtam on 20/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ draft/draftApp.swift
@@ -0,0 +1,17 @@
+//
+//  draftApp.swift
+//  draft
+//
+//  Created by davidtam on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct draftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
